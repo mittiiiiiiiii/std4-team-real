f99c60de2671953dda2ac7a7daafd9bc15037bf9
Mitiii <srp5628comust@gmail.com>
2025-04-09 11:18:46 +0900

    線の入力まで作成

--- phase_1/task1.c
+++ phase_1/task1.c
@@ -9,10 +9,22 @@ typedef struct {
 }Segment;
 
 int main(){
-    int N,M,P,Q;
+    int N,M,P,Q,i;
 
     scanf("%d %d %d %d",&N,&M,&P,&Q);
 
+    Point points[N];
+    for(i=0;i<N;i++){
+        scanf("%lf %lf",&points[i].x,&points[i].y);
+    }
+
+    int a,b=0;
+    Segment segments[M];
+    for(i=0;i<M;i++){
+        scanf("%d %d",&a,&b);
+        segments[i].p1=points[a-1];
+        segments[i].p2=points[b-1];
+    }
 
     return 0;
 }
