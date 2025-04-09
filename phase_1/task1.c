65b8749161a0719db2d829d19d4c819575aed4b6
Mitiii <srp5628comust@gmail.com>
2025-04-09 11:04:25 +0900

    inputを作る

---
+++ phase_1/task1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct {
+    double x, y;
+}Point;
+
+typedef struct {
+    Point p1, p2;
+}Segment;
+
+int main(){
+    int N,M,P,Q;
+
+    scanf("%d %d %d %d",&N,&M,&P,&Q);
+
+
+    return 0;
+}
