43f537d1d9c23f0606f27699f256a6f54fe2347b
Mitiii <srp5628comust@gmail.com>
2025-04-09 11:38:28 +0900

    動作を確認できた

--- phase_1/task1.c
+++ phase_1/task1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 typedef struct {
     double x, y;
@@ -9,7 +10,6 @@ typedef struct {
 }Segment;
 
 int findIntersection(Segment s1,Segment s2,Point *intersection) {
-    // 係数を計算
     double a1=s1.p2.y-s1.p1.y;
     double b1=s1.p1.x-s1.p2.x;
     double c1=a1*s1.p1.x+b1*s1.p1.y;
@@ -18,27 +18,23 @@ int findIntersection(Segment s1,Segment s2,Point *intersection) {
     double b2=s2.p1.x-s2.p2.x;
     double c2=a2*s2.p1.x+b2*s2.p1.y;
 
-    // 行列式を計算
     double determinant=a1*b2-a2*b1;
 
-    // 行列式が0の場合は平行または重なる
     if(fabs(determinant)<1e-9){
-        return 0; // 交差なし
+        return 0;
     }
 
-    // 交差地点を計算
     intersection->x=(b2*c1-b1*c2)/determinant;
     intersection->y=(a1*c2-a2*c1)/determinant;
 
-    // 線分上に交差地点があるかを確認
-    if(intersection->x<fmin(s1.p1.x, s1.p2.x)||intersection->x>fmax(s1.p1.x,s1.p2.x)||
-        intersection->x<fmin(s2.p1.x, s2.p2.x)||intersection->x>fmax(s2.p1.x,s2.p2.x)||
-        intersection->y<fmin(s1.p1.y, s1.p2.y)||intersection->y>fmax(s1.p1.y,s1.p2.y)||
-        intersection->y<fmin(s2.p1.y, s2.p2.y)||intersection->y>fmax(s2.p1.y,s2.p2.y)){
-        return 0; // 線分上にない場合は交差なし
+    if(intersection->x < fmin(s1.p1.x, s1.p2.x)||intersection->x > fmax(s1.p1.x,s1.p2.x)||
+        intersection->x < fmin(s2.p1.x, s2.p2.x)||intersection->x > fmax(s2.p1.x,s2.p2.x)||
+        intersection->y < fmin(s1.p1.y, s1.p2.y)||intersection->y > fmax(s1.p1.y,s1.p2.y)||
+        intersection->y < fmin(s2.p1.y, s2.p2.y)||intersection->y > fmax(s2.p1.y,s2.p2.y)){
+        return 0;
     }
 
-    return 1; // 交差あり
+    return 1;
 }
 
 int main(){
@@ -65,7 +61,7 @@ int main(){
     }
 
     Point intersection;
-    if(findIntersection(segments[0],segments[1],&intersection)){//2本の線分について交差判定
+    if(findIntersection(segments[0],segments[1],&intersection)){
         printf("%.5lf %.5lf",intersection.x,intersection.y);
     } else {
         printf("No intersection\n");
